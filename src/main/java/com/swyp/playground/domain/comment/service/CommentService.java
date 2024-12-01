3af42ee46b11d7df34694b6d25580813d9626a9e
Spooder <yoshi7591@naver.com>
2024-12-01 14:31:37 +0900

    feat: 댓글 CRUD MVP 구현

--- src/main/java/com/swyp/playground/domain/comment/service/CommentService.java
+++ src/main/java/com/swyp/playground/domain/comment/service/CommentService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.swyp.playground.domain.comment.domain.Comment;
+import com.swyp.playground.domain.comment.dto.WriteCommentDto;
 import com.swyp.playground.domain.comment.repository.CommentRepository;
+
 
 @Service
 public class CommentService {
@@ -15,10 +17,12 @@ public class CommentService {
     @Autowired
     private CommentRepository commentRepository;
 
+    /* -- POST -- */
     public Comment writeComment(Comment comment) {
         return commentRepository.save(comment);
     }
 
+    /* -- GET -- */
     public List<Comment> getAllComments() {
         return commentRepository.findAll();
     }
@@ -27,6 +31,23 @@ public class CommentService {
         return commentRepository.findById(id);
     }
 
+    /* -- UPDATE -- */
+    public void patchCommentById(Long id, WriteCommentDto newComment) {
+        try {
+            Comment prevComment = commentRepository.findById(id)
+                            .orElseThrow(() -> new NullPointerException("해당 댓글이 존재하지 않습니다."));
+            
+            if (prevComment.getContent() != newComment.getContent()) {
+                prevComment.setContent(newComment.getContent());
+            }
+            
+            commentRepository.save(prevComment);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    /* -- DELETE -- */
     public void deleteComment(Long id) {
         commentRepository.deleteById(id);
     }
