7a6398d5ee2178caeb1c6a4e74d952e9469d9fc4
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-12-05 22:34:15 +0900

    feat:ncp objectstorage add

---
+++ src/main/java/com/swyp/playground/common/S3/ProfileController.java
@@ -0,0 +1,28 @@
+package com.swyp.playground.common.S3;
+
+
+import com.swyp.playground.domain.parent.service.ParentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/parent")
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ParentService parentService;
+
+    @PostMapping("/{parentId}/upload-profile")
+    public ResponseEntity<String> uploadProfileImage(
+            @PathVariable Long parentId,
+            @RequestParam("file") MultipartFile file) {
+        try {
+            String fileUrl = parentService.uploadProfileImage(parentId, file);
+            return ResponseEntity.ok(fileUrl); // 업로드된 이미지 URL 반환
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("이미지 업로드 실패: " + e.getMessage());
+        }
+    }
+}
